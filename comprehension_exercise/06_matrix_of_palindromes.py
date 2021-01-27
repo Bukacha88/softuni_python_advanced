edfe5c55a779227e120c694280a4e428623f7d46
Bukacha88 <71729802+Bukacha88@users.noreply.github.com>
2021-01-27 13:43:18 +0200

    Add files via upload

---
+++ comprehension_exercise/06_matrix_of_palindromes.py
@@ -0,0 +1,4 @@
+rows, cols = [int(el) for el in input().split()]
+result = [[f"{chr(num)}{chr(nested_num)}{chr(num)}" for nested_num in range(num, num+cols)]for num in range(97,
+                                                                                                            97+rows)]
+[print(*row) for row in result]
