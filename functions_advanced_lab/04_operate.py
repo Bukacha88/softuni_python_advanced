74f81f1713d8f4738e028470de9565e4120a4810
Bukacha88 <71729802+Bukacha88@users.noreply.github.com>
2021-01-27 18:10:49 +0200

    Add files via upload

---
+++ functions_advanced_lab/04_operate.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+mapper = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
+    "/": lambda x, y: x / y,
+
+}
+
+
+def operate(operator, *args):
+    return reduce(mapper[operator], args)
