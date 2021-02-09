a99ff24e9bf8f558557ecd714ef94156afe5df6e
Ivan <mzifa@abv.bg>
2021-02-10 00:13:18 +0200

    update

--- advancedexam_preperation/python_advanced_exam_24_october_2020/02_checkmate.py
+++ advancedexam_preperation/python_advanced_exam_24_october_2020/02_checkmate.py
@@ -1,3 +1,31 @@
+def is_valid(r, c, ):
+    if 0 <= r < 8 and 0 <= c < 8:
+        return True
+    return False
+
+
+def count_queens(the_matrix, row_1, col_1, queens_count):
+    moves = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    queens_count = []
+    curr_row = row_1
+    curr_col = col_1
+    for i in range(len(moves)):
+
+        while True:
+            curr_row += moves[i][0]
+            curr_col += moves[i][1]
+            if is_valid(curr_row, curr_col):
+                if the_matrix[curr_row][curr_col] == 'Q':
+                    queens_count.append([curr_row, curr_col])
+                    curr_row = row_1
+                    curr_col = col_1
+                    break
+            else:
+                curr_row = row_1
+                curr_col = col_1
+                break
+    return queens_count
+
 matrix = []
 queens = []
 kings_row = 0
@@ -11,75 +39,9 @@ for i in range(len(matrix)):
             kings_col = j
 current_row = kings_row
 current_col = kings_col
-if current_col < 7:
-    while current_col < 7:
-        current_col += 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
-current_row = kings_row
-current_col = kings_col
-if current_col > 0:
-    while current_col > 0:
-        current_col -= 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
-current_row = kings_row
-current_col = kings_col
-if current_row < 7:
-    while current_row < 7:
-        current_row += 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
-current_row = kings_row
-current_col = kings_col
-if current_row > 0:
-    while current_row > 0:
-        current_row -= 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
-current_row = kings_row
-current_col = kings_col
-if current_row < 7 and current_col < 7:
-    while current_row < 7 and current_col < 7:
-        current_row += 1
-        current_col += 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
-current_row = kings_row
-current_col = kings_col
-if current_row > 0 and current_col > 0:
-    while current_row > 0 and current_col > 0:
-        current_row -= 1
-        current_col -= 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
-current_row = kings_row
-current_col = kings_col
-if current_row > 0 and current_col < 7:
-    while current_row > 0 and current_col < 7:
-        current_row -= 1
-        current_col += 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
-current_row = kings_row
-current_col = kings_col
-if current_row < 7 and current_col > 0:
-    while current_row < 7 and current_col > 0:
-        current_row += 1
-        current_col -= 1
-        if matrix[current_row][current_col] == 'Q':
-            queens.append([current_row, current_col])
-            break
 
-
-if queens:
+queens = count_queens(matrix, current_row, current_col, queens)
+if len(queens) > 0:
     for queen in queens:
         print(queen)
 else:
